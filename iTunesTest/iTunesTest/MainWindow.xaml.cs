23aed32e5bad625e7efc6bb0f6bd3e9de6286ede
Matt Stanford <mds6058@gmail.com>
2013-11-07 23:56:21 -0500

    Added Json.NET to project

--- iTunesTest/iTunesTest/MainWindow.xaml.cs
+++ iTunesTest/iTunesTest/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace iTunesTest
 {
@@ -24,5 +26,17 @@ namespace iTunesTest
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String url = "https://itunes.apple.com/search?term=karma%20police&attribute=songTerm&entity=album";
+            var json = new WebClient().DownloadString(url);
+
+           // Dictionary<string, string> deserialized = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            TestLabel.Content = "Clicked!";
+
+        }
+
     }
 }
